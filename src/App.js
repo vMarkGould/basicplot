bbcd433f82a61ef2456b6607b3a7d2cc35d2fb8d
EC2 Default User <ec2-user@ip-172-31-36-145.ap-southeast-2.compute.internal>
2021-09-12 04:38:00 +0000

    small update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
       <Plot
         data={[
           {
-            x: [1, 2, 3],
+            x: [2, 2, 3],
             y: [2, 6, 3],
             type: 'scatter',
             mode: 'lines+markers',
